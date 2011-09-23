7d178603aca60f3919d4a854c60d6a625d1fb050
kdesevis@gmail.com <kdesevis@gmail.com@f2112cfc-e58d-1abd-99e2-74778cf860b3>
2011-09-23 08:54:12 +0000

    Splits the work into parts, but fails to actually do it.

---
+++ trunk/Mandelbrot/ConvergenceCheckResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mandelbrot
+{
+    struct ConvergenceCheckResult
+    {
+        public int iterations;
+        public double x;
+        public double y;
+
+        public ConvergenceCheckResult(int it, double x, double y)
+        {
+            this.iterations = it;
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
