5831c6e76f78cc51d9ce650ceb21be1f76f1aedb
roelvdbroek@gmail.com <roelvdbroek@gmail.com@f2112cfc-e58d-1abd-99e2-74778cf860b3>
2011-10-01 21:42:40 +0000

    Added a menuStrip, containing save, reset and close buttons

--- trunk/Mandelbrot/MainWindow.Designer.cs
+++ trunk/Mandelbrot/MainWindow.Designer.cs
@@ -38,16 +38,23 @@
             this.statusStrip = new System.Windows.Forms.StatusStrip();
             this.statusStripSizeLabel = new System.Windows.Forms.ToolStripStatusLabel();
             this.statusStripTimeLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.menuStripStartList = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStripStartNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStripStartSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStripStartExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveImageDialog = new System.Windows.Forms.SaveFileDialog();
             this.scaleTextBox = new Mandelbrot.DoubleBox();
             this.centreYTextBox = new Mandelbrot.DoubleBox();
             this.centreXTextBox = new Mandelbrot.DoubleBox();
             ((System.ComponentModel.ISupportInitialize)(this.mandelImageContainer)).BeginInit();
             this.statusStrip.SuspendLayout();
+            this.menuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
             // generateImageButton
             // 
-            this.generateImageButton.Location = new System.Drawing.Point(401, 49);
+            this.generateImageButton.Location = new System.Drawing.Point(445, 63);
             this.generateImageButton.Name = "generateImageButton";
             this.generateImageButton.Size = new System.Drawing.Size(75, 23);
             this.generateImageButton.TabIndex = 0;
@@ -58,7 +65,7 @@
             // mandelImageContainer
             // 
             this.mandelImageContainer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.mandelImageContainer.Location = new System.Drawing.Point(20, 80);
+            this.mandelImageContainer.Location = new System.Drawing.Point(20, 100);
             this.mandelImageContainer.Name = "mandelImageContainer";
             this.mandelImageContainer.Size = new System.Drawing.Size(500, 500);
             this.mandelImageContainer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
@@ -71,46 +78,46 @@
             // centreXLabel
             // 
             this.centreXLabel.AutoSize = true;
-            this.centreXLabel.Location = new System.Drawing.Point(21, 24);
+            this.centreXLabel.Location = new System.Drawing.Point(21, 42);
             this.centreXLabel.Name = "centreXLabel";
-            this.centreXLabel.Size = new System.Drawing.Size(54, 13);
+            this.centreXLabel.Size = new System.Drawing.Size(55, 13);
             this.centreXLabel.TabIndex = 2;
-            this.centreXLabel.Text = "midden X:";
+            this.centreXLabel.Text = "Midden X:";
             // 
             // centreYLabel
             // 
             this.centreYLabel.AutoSize = true;
-            this.centreYLabel.Location = new System.Drawing.Point(21, 55);
+            this.centreYLabel.Location = new System.Drawing.Point(21, 73);
             this.centreYLabel.Name = "centreYLabel";
-            this.centreYLabel.Size = new System.Drawing.Size(54, 13);
+            this.centreYLabel.Size = new System.Drawing.Size(55, 13);
             this.centreYLabel.TabIndex = 4;
-            this.centreYLabel.Text = "midden Y:";
+            this.centreYLabel.Text = "Midden Y:";
             // 
             // scaleLabel
             // 
             this.scaleLabel.AutoSize = true;
-            this.scaleLabel.Location = new System.Drawing.Point(212, 24);
+            this.scaleLabel.Location = new System.Drawing.Point(228, 42);
             this.scaleLabel.Name = "scaleLabel";
-            this.scaleLabel.Size = new System.Drawing.Size(41, 13);
+            this.scaleLabel.Size = new System.Drawing.Size(43, 13);
             this.scaleLabel.TabIndex = 6;
-            this.scaleLabel.Text = "schaal:";
+            this.scaleLabel.Text = "Schaal:";
             // 
             // maxIterationsTextBox
             // 
-            this.maxIterationsTextBox.Location = new System.Drawing.Point(264, 52);
+            this.maxIterationsTextBox.Location = new System.Drawing.Point(280, 70);
             this.maxIterationsTextBox.Name = "maxIterationsTextBox";
-            this.maxIterationsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.maxIterationsTextBox.Size = new System.Drawing.Size(115, 20);
             this.maxIterationsTextBox.TabIndex = 9;
             this.maxIterationsTextBox.Text = "500";
             // 
             // maxIterationsLabel
             // 
             this.maxIterationsLabel.AutoSize = true;
-            this.maxIterationsLabel.Location = new System.Drawing.Point(212, 55);
+            this.maxIterationsLabel.Location = new System.Drawing.Point(228, 73);
             this.maxIterationsLabel.Name = "maxIterationsLabel";
-            this.maxIterationsLabel.Size = new System.Drawing.Size(46, 13);
+            this.maxIterationsLabel.Size = new System.Drawing.Size(47, 13);
             this.maxIterationsLabel.TabIndex = 8;
-            this.maxIterationsLabel.Text = "iteraties:";
+            this.maxIterationsLabel.Text = "Iteraties:";
             // 
             // statusStrip
             // 
@@ -118,7 +125,7 @@
             this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.statusStripSizeLabel,
             this.statusStripTimeLabel});
-            this.statusStrip.Location = new System.Drawing.Point(0, 600);
+            this.statusStrip.Location = new System.Drawing.Point(0, 620);
             this.statusStrip.Name = "statusStrip";
             this.statusStrip.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.statusStrip.Size = new System.Drawing.Size(540, 20);
@@ -143,30 +150,84 @@
             this.statusStripTimeLabel.Size = new System.Drawing.Size(110, 15);
             this.statusStripTimeLabel.Text = "Generated in: ms";
             // 
+            // menuStrip
+            // 
+            this.menuStrip.BackColor = System.Drawing.SystemColors.Control;
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuStripStartList});
+            this.menuStrip.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.Flow;
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.menuStrip.Size = new System.Drawing.Size(540, 23);
+            this.menuStrip.TabIndex = 12;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // menuStripStartList
+            // 
+            this.menuStripStartList.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.menuStripStartList.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuStripStartNew,
+            this.menuStripStartSave,
+            this.menuStripStartExit});
+            this.menuStripStartList.Name = "menuStripStartList";
+            this.menuStripStartList.Size = new System.Drawing.Size(43, 19);
+            this.menuStripStartList.Text = "Start";
+            // 
+            // menuStripStartNew
+            // 
+            this.menuStripStartNew.Name = "menuStripStartNew";
+            this.menuStripStartNew.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.menuStripStartNew.Size = new System.Drawing.Size(218, 22);
+            this.menuStripStartNew.Text = "Nieuw";
+            this.menuStripStartNew.Click += new System.EventHandler(this.resetImage);
+            // 
+            // menuStripStartSave
+            // 
+            this.menuStripStartSave.Name = "menuStripStartSave";
+            this.menuStripStartSave.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.menuStripStartSave.Size = new System.Drawing.Size(218, 22);
+            this.menuStripStartSave.Text = "Afbeelding Opslaan";
+            this.menuStripStartSave.Click += new System.EventHandler(this.saveImage);
+            // 
+            // menuStripStartExit
+            // 
+            this.menuStripStartExit.Name = "menuStripStartExit";
+            this.menuStripStartExit.Size = new System.Drawing.Size(218, 22);
+            this.menuStripStartExit.Text = "Afsluiten";
+            this.menuStripStartExit.Click += new System.EventHandler(this.exitApplication);
+            // 
+            // saveImageDialog
+            // 
+            this.saveImageDialog.DefaultExt = "bmp";
+            this.saveImageDialog.FileName = "MandelBrot";
+            this.saveImageDialog.Filter = "Bitmap Afbeelding|*.bmp|JPeg Afbeelding|*.jpg|Gif Afbeelding|*.gif";
+            this.saveImageDialog.Title = "Afbeelding Opslaan";
+            // 
             // scaleTextBox
             // 
             this.scaleTextBox.Double = 0.01D;
-            this.scaleTextBox.Location = new System.Drawing.Point(264, 21);
+            this.scaleTextBox.Location = new System.Drawing.Point(280, 39);
             this.scaleTextBox.Name = "scaleTextBox";
-            this.scaleTextBox.Size = new System.Drawing.Size(100, 20);
+            this.scaleTextBox.Size = new System.Drawing.Size(115, 20);
             this.scaleTextBox.TabIndex = 7;
             this.scaleTextBox.Text = "0.01";
             // 
             // centreYTextBox
             // 
             this.centreYTextBox.Double = 0D;
-            this.centreYTextBox.Location = new System.Drawing.Point(81, 52);
+            this.centreYTextBox.Location = new System.Drawing.Point(81, 70);
             this.centreYTextBox.Name = "centreYTextBox";
-            this.centreYTextBox.Size = new System.Drawing.Size(100, 20);
+            this.centreYTextBox.Size = new System.Drawing.Size(114, 20);
             this.centreYTextBox.TabIndex = 5;
             this.centreYTextBox.Text = "0.0";
             // 
             // centreXTextBox
             // 
             this.centreXTextBox.Double = 0D;
-            this.centreXTextBox.Location = new System.Drawing.Point(81, 21);
+            this.centreXTextBox.Location = new System.Drawing.Point(81, 39);
             this.centreXTextBox.Name = "centreXTextBox";
-            this.centreXTextBox.Size = new System.Drawing.Size(100, 20);
+            this.centreXTextBox.Size = new System.Drawing.Size(114, 20);
             this.centreXTextBox.TabIndex = 3;
             this.centreXTextBox.Text = "0.0";
             // 
@@ -175,8 +236,9 @@
             this.AcceptButton = this.generateImageButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(540, 620);
+            this.ClientSize = new System.Drawing.Size(540, 640);
             this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.menuStrip);
             this.Controls.Add(this.maxIterationsTextBox);
             this.Controls.Add(this.maxIterationsLabel);
             this.Controls.Add(this.scaleTextBox);
@@ -187,7 +249,8 @@
             this.Controls.Add(this.centreXLabel);
             this.Controls.Add(this.mandelImageContainer);
             this.Controls.Add(this.generateImageButton);
-            this.MinimumSize = new System.Drawing.Size(556, 658);
+            this.MainMenuStrip = this.menuStrip;
+            this.MinimumSize = new System.Drawing.Size(556, 678);
             this.Name = "MainWindow";
             this.Text = "Mandelbrot Generator";
             this.ResizeBegin += new System.EventHandler(this.setResizeFlag);
@@ -196,6 +259,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.mandelImageContainer)).EndInit();
             this.statusStrip.ResumeLayout(false);
             this.statusStrip.PerformLayout();
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -216,6 +281,12 @@
         private System.Windows.Forms.StatusStrip statusStrip;
         private System.Windows.Forms.ToolStripStatusLabel statusStripTimeLabel;
         private System.Windows.Forms.ToolStripStatusLabel statusStripSizeLabel;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem menuStripStartList;
+        private System.Windows.Forms.ToolStripMenuItem menuStripStartNew;
+        private System.Windows.Forms.ToolStripMenuItem menuStripStartSave;
+        private System.Windows.Forms.ToolStripMenuItem menuStripStartExit;
+        private System.Windows.Forms.SaveFileDialog saveImageDialog;
 
     }
 }
