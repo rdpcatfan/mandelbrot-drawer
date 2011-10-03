51651529c521f238563b3f57961a69f57ff952f3
kdesevis@gmail.com <kdesevis@gmail.com@f2112cfc-e58d-1abd-99e2-74778cf860b3>
2011-10-03 21:58:54 +0000

    Started working on ColourMixer.

---
+++ trunk/Mandelbrot/ColourChoice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mandelbrot
+{
+    class ColourChoice : UserControl
+    {
+        private IList<PictureBox> colours;
+    }
+}
